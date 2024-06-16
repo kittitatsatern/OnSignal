224e38564ac646fb8ae3a25767fac0d72610f66b
Kittitat Satern <Kittitat.main@gmail.com>
2024-06-16 19:04:42 +0700

    Rewrite
    rewrite Signal
    
    add double linked list for test performance.

--- SimpleSignalDoubleLinkedList.py
+++ SimpleSignalDoubleLinkedList.py
@@ -3,6 +3,7 @@ class Connection:
         self.signal = signal
         self.connected = True
         self.callback = callback
+        self.prev = None
         self.next = None
 
     def disconnect(self):
@@ -10,12 +11,14 @@ class Connection:
         if self.signal:
             if self.signal.head == self:
                 self.signal.head = self.next
-            else:
-                prev = self.signal.head
-                while prev and prev.next != self:
-                    prev = prev.next
-                if prev:
-                    prev.next = self.next
+            if self.signal.tail == self:
+                self.signal.tail = self.prev
+            if self.prev:
+                self.prev.next = self.next
+            if self.next:
+                self.next.prev = self.prev
+            self.prev = None
+            self.next = None
 
     def __str__(self):
         return "Connection"
@@ -23,28 +26,29 @@ class Connection:
 class Signal:
     def __init__(self) -> None:
         self.head = None
-    
+        self.tail = None
+
     def connect(self, callback):
         connection = Connection(self, callback)
-        if self.head is None:
-            self.head = connection
+        if self.tail:
+            self.tail.next = connection
+            connection.prev = self.tail
         else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = connection
+            self.head = connection
+        self.tail = connection
         return connection
-    
+
     def wait(self, callback):
         def wait_callback(*args):
             callback(*args)
             wait_connection.disconnect()
         wait_connection = self.connect(wait_callback)
         return wait_connection
-    
+
     def disconnect_all(self):
         self.head = None
-    
+        self.tail = None
+
     def fire(self, *args):
         connection = self.head
         while connection:
--- example.py
+++ example.py
@@ -4,17 +4,23 @@ from SimpleSignal import Signal
 def callback(message):
     print(f"message: {message}")
 
+def callback2(message):
+    print(f"message2: {message}")
+
 # Create an instance of Signal
 signal = Signal()
 
 # Connect the callback function
-signal.connect(callback)
+connection = signal.connect(callback)
+signal.connect(callback2)
+
+connection.disconnect()
 
 # Fire the signal
 signal.fire("Hello World.")
 
 # Disconnect the callback
-signal.disconnect()
+signal.disconnect_all()
 
 # Trying to fire again won't do anything
 signal.fire("This won't be received.")
