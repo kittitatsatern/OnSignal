f9c2b6fc82a4e1683a9d316e73edddc6df5ee9d1
Kittitat Satern <Kittitat.main@gmail.com>
2024-06-16 13:12:05 +0700

    added "wait" function
    wait for a single fire event and then disconnect the callback.

--- SimpleSignal.py
+++ SimpleSignal.py
@@ -2,23 +2,32 @@ class Signal:
     def __init__(self):
         self.callback = None
 
-    # connect a callback function to the signal
+    # connect a callback function to the signal.
     def connect(self, callback):
-        if not self.callback:
-            self.callback = callback
+        self.callback = callback
+
+    # wait for a single fire event and then disconnect the callback.
+    def wait(self, callback):
+        self.connect(callback)
+        self.waiting = True
             
-    # disconnects currently connected callback
+    # disconnect currently connected callback.
     def disconnect(self):
-        if self.callback:
-            self.callback = None
+        self.callback = None
+        if self.waiting:
+            self.waiting = None
 
     # trigger the connected callback function.
     def fire(self, *args, **kwargs):
-        if self.callback:
+         if self.callback:
             self.callback(*args, **kwargs)
+            if self.waiting:
+                self.disconnect()
 
-    # fire the signal once and then disconnect the callback
+    # fire the signal once and then disconnect the callback.
     def once(self, *args, **kwargs):
-        if self.callback:
-            self.fire(*args, **kwargs)
-            self.disconnect()
+        self.fire(*args, **kwargs)
+        self.disconnect()
+    
+    def __str__(self):
+        return "Signal"
